dd18ff7dd9850ce836505e48c4792abeec52a92d
mohamed mostafa <167251907+mohamed2678@users.noreply.github.com>
2025-04-10 03:11:34 +0200

    Update vite.config.ts
    edit old issue

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      // إضافة هذا السطر لاستخدام البناء الصحيح لـ Vue
       'vue': 'vue/dist/vue.esm-bundler.js',
     },
   },
